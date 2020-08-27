a0169c53055dd21dbab73a5073c619eef1e15ad9
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-27 22:31:24 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  add = num1.to_i + num2.to_i
+  return add
 end
 
 def subtraction(num1, num2)
-
+  subt = num1.to_i - num2.to_i
+  return subt
 end
 
 def division(num1, num2)
-
+  div = num1.to_i/num2.to_i
+  return div
 end
 
 def multiplication(num1, num2)
-
+  mul = num1.to_i*num2.to_i
+  return mul
 end
 
 def modulo(num1, num2)
-
+  mod = num1.to_i % num2.to_i
+  return mod
 end
 
 def square_root(num)
-
+  sqr = Math.sqrt(num.to_i)
+  return sqr
 end
